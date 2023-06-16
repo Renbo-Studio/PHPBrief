b8fbc8942984b885f213571afe90fd34e296034d
Joshua Umahi <98918202+Renbo-Studio@users.noreply.github.com>
2023-06-16 13:17:44 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php  
+    echo "I Never Chop Pizza For My Life";
+    echo "<br> The One I Chop Na Garri Them Use Make Am <br>"
+    //& I'm Not Joking lol
+    /* Multiline Comment */
+?>
+
+<!-- VARIABLES  -->
+
+<?php 
+    $name = "Nelson";
+    $food = 'Cheese';
+    $email = 'nelsonDeCheeseLover@fake.com';
+
+    echo "Hello {$name}, your best food is {$food} <br>";
+
+    echo "Your info is stored in your email, {$email}";
+?>
+
